1898bbfe414118166e1fe39041c42948390ea443
Venkat_Kuppoor <venkkuppoor@gmail.com>
2023-03-31 21:49:42 -0400

    fixed pacman-ghost_in_range

--- P2/app/src/main/java/pacman/PacMan.java
+++ P2/app/src/main/java/pacman/PacMan.java
@@ -47,10 +47,10 @@ public class PacMan {
   public boolean is_ghost_in_range() {
     for (int dx = -1; dx <= 1; dx++) {
       for (int dy = -1; dy <= 1; dy++) {
-        Location newLocation = myLoc.shift(dy, dx);
+        Location newLocation = myLoc.shift(dx, dy);
 
-        if (myMap.getLoc(newLocation).contains(Map.Type.PACMAN))
-          return false;
+        if (myMap.getLoc(newLocation).contains(Map.Type.GHOST))
+          return true;
       }
     }
 
